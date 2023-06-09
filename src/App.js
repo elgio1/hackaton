08c16dcb5ba6470a603c8fb3ac1e6d172ec17df2
corazon03 <82195740+corazon03@users.noreply.github.com>
2023-06-08 20:49:48 -0600

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { inicio } from './componentes/inicio';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <inicio></inicio>
         <a
           className="App-link"
           href="https://reactjs.org"

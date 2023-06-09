7f0d348832c375ccfe8c9df043ece71b3b1d31cd
corazon03 <82195740+corazon03@users.noreply.github.com>
2023-06-08 21:03:56 -0600

    Inicio

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola</p>
-        <p>Hola</p>
+        <inicio />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/componentes/inicio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const inicio = () => {
+  return (
+    <div>
+        <form>
+            <input type='text' placeholder='Usuario'></input>
+            <input type='password' placeholder='Contraseña'></input>
+        </form>
+    </div>
+  )
+}
